094e4199fa27fb3b942f27408ceab965a8ee19cb
SafaMarley <safaktrcgl@gmail.com>
2021-08-01 16:35:43 +0300

    Did some of the challenges in Project

--- CreateWithCodeProjects/Prototypes/JuniorProgrammer/Assets/Scripts/UI/UIMainScene.cs
+++ CreateWithCodeProjects/Prototypes/JuniorProgrammer/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void ExitToTheMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
